25149199eba5267cf7912d4b58c251f06f8039a3
AhmedAlobaid <ahmed.obaid44@gmail.com>
2026-07-14 01:07:16 +0300

    Delete lib/db.ts

--- lib/db.ts
+++
@@ -1,6 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "../db/schema";
-
-export const db = drizzle(neon(process.env.NETLIFY_DATABASE_URL!), { schema });
-export { schema };
